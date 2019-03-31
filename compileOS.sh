e5bf1e39cd5f93ceee8ad976f5e41774b64feab7
Valentino Simamora <43459761+nnadnad@users.noreply.github.com>
2019-03-31 20:50:24 +0700

    Update compileOS.sh

--- compileOS.sh
+++ compileOS.sh
@@ -8,13 +8,17 @@ dd if=sectors.img of=floppya.img bs=512 count=1 seek=259 conv=notrunc
 dd if=bootload of=floppya.img bs=512 count=1 conv=notrunc
 dd if=map.img of=floppya.img bs=512 count=1 seek=1 conv=notrunc
 dd if=dir.img of=floppya.img bs=512 count=1 seek=2 conv=notrunc
+
 bcc -ansi -c -o kernel.o kernel.c
 as86 kernel.asm -o kernel_asm.o
 ld86 -o kernel -d kernel.o kernel_asm.o
 dd if=kernel of=floppya.img bs=512 conv=notrunc seek=3
+
+as86 lib.asm -o lib_asm
+
 bcc -ansi -c -o shell.o shell.c
-as86 lib.asm -o lib.o
-ld86 -o shell -d shell.o lib.o 
+ld86 -o shell -d shell.o lib_asm.o
+./loadFile shell
 
 
 ./loadFile keyproc2
