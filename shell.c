93eef5673f74aabe78c2dda596f121a1325201e6
Valentino Simamora <43459761+nnadnad@users.noreply.github.com>
2019-04-01 20:20:29 +0700

    Update shell with command cd

--- shell.c
+++ shell.c
@@ -1,25 +1,48 @@
 // #define MAX_BYTE 256
 #define SECTOR_SIZE 512
 #define NOT_FOUND -1
+#define DIRS_SECTOR 0x101
 int stringCmp(char *a, char *b, int len);
 
 
 void main() {
-    char curDir, useDir;
+    char currentDir, useDir;
     char *argv[16];
     char argc;
-    char input[512];
+    char input[SECTOR_SIZE];
     char string[16][128];
     char dir[SECTOR_SIZE];
-    int success=1, offset;
+    int success=1, offset, newIdx;
     int se[4], i,j,temp;
-    interrupt(0x21, 0x21, &curDir, 0, 0);
+    interrupt(0x21, 0x21, &currentDir, 0, 0);
         // read input
         interrupt(0x21, 0x00, "$ ", 0, 0);
         interrupt(0x21, 0x01, input, 0, 0);
         interrupt(0x21, 0x00, "\r\n", 0, 0);
-        if(0){
-            interrupt(0x21,0x00,"cd\r\n",0,0);
+        // if(0){
+        //     interrupt(0x21,0x00,"cd\r\n",0,0);
+        // }
+        if (strcmp(input, "cd", 2)) {
+            offset = 2;
+            newIdx = offset;
+            while (input[newIdx] == ' ') { //untuk melewati space antara command cd dan argumen selanjutnya
+                newIdx++;
+            }
+            if (input[newIdx] == 0) {
+                interrupt(0x21, 0x20, 0xFF, 0, 0); // jika hanya argumen cd, akan kembali ke root directory
+            } else {
+                if (se[2] == NOT_FOUND) {
+                    interrupt(0x21, 0x00, "Tidak ada direktori tersebut.\r\n", 0, 0);
+                } else {
+                    interrupt(0x21, 0x02, dir, DIRS_SECTOR, 0);
+                    if (se[2] == NOT_FOUND) {
+                        interrupt(0x21, 0x00, "Tidak ada direktori.\r\n",0,0);
+                    } else {
+                        currentDir = (char) se[2];
+                        interrupt(0x21, 0x20, currentDir, 0, 0);
+                    }
+                }
+            }
         }
         else if(input[0]!=0){
             // Prepare the argument
@@ -29,7 +52,7 @@ void main() {
             j = 0;
             temp = 0;
             if(strcmp(input, "./", 2)){
-                useDir = curDir;
+                useDir = currentDir;
                 offset = 2;
                 temp = offset;
             }
@@ -55,7 +78,7 @@ void main() {
                     offset++;
                 }
             }
-            interrupt(0x21,0x20,curDir,argc,argv);
+            interrupt(0x21,0x20,currentDir,argc,argv);
             offset = temp;
             for(i=offset;input[i]!=' ' && input[i]!='\0';i++);
             input[i] = '\0';
@@ -70,6 +93,8 @@ void main() {
         interrupt(0x21,0x07,&i,0,0);
 
     }
+
+
 int strcmp(char *a, char *b, int len) {
     int i = 0;
     while (i < len) {
