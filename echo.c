ee58e2fb565e35fe8421593e7c01b644c03253df
putugde <42746060+putugde@users.noreply.github.com>
2019-04-01 19:56:05 +0700

    echontol

--- echo.c
+++ echo.c
@@ -1,17 +1,21 @@
 int main(){
 	int result;
-	char argc;
+	char argc,i;
 	char *argv[10];
+    i = 0;
     interrupt(0x21, 0x22, &argc, 0, 0);
-
-	while(argc > 1){
-		interrupt(0x21, 0x23, 0, argv, 0);
-		interrupt(0x21, 0x00, argv, 0, 0);
-		interrupt(0x21, 0x00, " ",0,0);
-		argc--;
-	} 
-	interrupt(0x21,0x23,0,argv,0);
-	interrupt(0x21,0x00,argv,0,0);
+    if (argc>1){
+            while(i < argc){
+            interrupt(0x21, 0x23, i, argv, 0);
+            interrupt(0x21, 0x00, argv, 0, 0);
+            interrupt(0x21, 0x00, " ",0,0);
+            i++;
+	    } 
+    }
+	else{
+        interrupt(0x21,0x23,0,argv,0);
+	    interrupt(0x21,0x00,argv,0,0);
+    }
 	interrupt(0x21, 0x00, "\r\n", 0, 0);
 	interrupt(0x21, 0x07, &result, 0, 0);
 }
